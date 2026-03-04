b4afbefd7feac4742c5913be5dbc8d502ebd4ace
Andrey Lebedev <andreylab50@mail.ru>
2026-03-04 15:18:44 +0500

    вывод с условием

--- fourth.py
+++ fourth.py
@@ -3,10 +3,10 @@ import sqlite3
 connection = sqlite3.connect('my_database.db')
 cursor = connection.cursor()
 
-cursor.execute('SELECT * FROM Users')
-users = cursor.fetchall()
+cursor.execute('SELECT username, age FROM Users WHERE age > ?', (25,))
+results = cursor.fetchall()
 
-for user in users:
-    print(user)
+for row in results:
+    print(row)
 
 connection.close()
