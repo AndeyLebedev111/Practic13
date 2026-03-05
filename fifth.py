8a4b73f8ef0f1bccdf29be2aa7e87cb32884f68c
Andrey Lebedev <andreylab50@mail.ru>
2026-03-05 21:23:27 +0500

    управление транзакциями

--- fifth.py
+++ fifth.py
@@ -1,16 +1,11 @@
 import sqlite3
 
-connection = sqlite3.connect('my_database.db')
-cursor = connection.cursor()
-
-try:
-    cursor.execute('BEGIN')
+with sqlite3.connect('my_database.db') as connection:
+    cursor = connection.cursor()
     
-    cursor.execute('INSERT INTO Users (username, email) VALUES (?, ?)', ('user1', 'user1@example.com'))
-    cursor.execute('INSERT INTO Users (username, email) VALUES (?, ?)', ('user2', 'user2@example.com'))
-    
-    cursor.execute('COMMIT')
-except:
-    cursor.execute('ROLLBACK')
-
-connection.close()
+    try:
+        with connection:
+            cursor.execute('INSERT INTO Users (username, email) VALUES (?, ?)', ('user3', 'user3@example.com'))
+            cursor.execute('INSERT INTO Users (username, email) VALUES (?, ?)', ('user4', 'user4@example.com'))
+    except:
+        pass
