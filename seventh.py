04f1dd7eb9e4d05761b8d819935a13b1587e9cd0
Andrey Lebedev <andreylab50@mail.ru>
2026-03-05 21:29:59 +0500

    работа с индексами

--- seventh.py
+++ seventh.py
@@ -3,23 +3,7 @@ import sqlite3
 connection = sqlite3.connect('my_database.db')
 cursor = connection.cursor()
 
-cursor.execute('''
-    CREATE TABLE IF NOT EXISTS Users (
-        id INTEGER PRIMARY KEY,
-        username TEXT NOT NULL,
-        email TEXT NOT NULL,
-        age INTEGER,
-        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-    )
-''')
-
-cursor.execute('''
-    CREATE TRIGGER IF NOT EXISTS update_created_at
-    AFTER INSERT ON Users
-    BEGIN
-        UPDATE Users SET created_at = CURRENT_TIMESTAMP WHERE id = NEW.id;
-    END;
-''')
+cursor.execute('CREATE INDEX idx_username ON Users (username)')
 
 connection.commit()
 connection.close()
