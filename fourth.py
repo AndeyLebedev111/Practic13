2d4d6710041658817cfd097ecb208ac5a4d9216c
Andrey Lebedev <andreylab50@mail.ru>
2026-03-05 15:53:28 +0500

    avg

--- fourth.py
+++ fourth.py
@@ -3,8 +3,8 @@ import sqlite3
 connection = sqlite3.connect('my_database.db')
 cursor = connection.cursor()
 
-cursor.execute('SELECT SUM(age) FROM Users')
-total_age = cursor.fetchone()[0]
+cursor.execute('SELECT AVG(age) FROM Users')
+average_age = cursor.fetchone()[0]
 
-print('Общая сумма возрастов пользователей:', total_age)
+print('Средний возраст пользователей:', average_age)
 connection.close()
