fc5c44d9b158e1d2cd6a4ae299609775c87faf3f
Andrey Lebedev <andreylab50@mail.ru>
2026-03-05 15:54:14 +0500

    max

--- fourth.py
+++ fourth.py
@@ -3,8 +3,8 @@ import sqlite3
 connection = sqlite3.connect('my_database.db')
 cursor = connection.cursor()
 
-cursor.execute('SELECT MIN(age) FROM Users')
-min_age = cursor.fetchone()[0]
+cursor.execute('SELECT MAX(age) FROM Users')
+max_age = cursor.fetchone()[0]
 
-print('Минимальный возраст среди пользователей:', min_age)
+print('Максимальный возраст среди пользователей:', max_age)
 connection.close()
