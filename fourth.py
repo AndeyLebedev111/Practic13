4bc2e24156030ce553536b35c26e922fdf424bdf
Andrey Lebedev <andreylab50@mail.ru>
2026-03-05 15:49:45 +0500

    сортировка

--- fourth.py
+++ fourth.py
@@ -3,15 +3,10 @@ import sqlite3
 connection = sqlite3.connect('my_database.db')
 cursor = connection.cursor()
 
-cursor.execute('SELECT age, AVG(age) FROM Users GROUP BY age')
+cursor.execute('SELECT username, age FROM Users ORDER BY age DESC')
 results = cursor.fetchall()
 
 for row in results:
     print(row)
 
-cursor.execute('SELECT age, AVG(age) FROM Users GROUP BY age HAVING AVG(age) > ?', (30,))
-filtered_results = cursor.fetchall()
-for row in filtered_results:
-    print(row)
-    
 connection.close()
