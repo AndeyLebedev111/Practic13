a8af496489cbbee6568348a41d0c612c024e138d
Andrey Lebedev <andreylab50@mail.ru>
2026-03-05 21:25:57 +0500

    подготовленные запросы

---
+++ seventh.py
@@ -0,0 +1,13 @@
+import sqlite3
+
+connection = sqlite3.connect('my_database.db')
+cursor = connection.cursor()
+
+query = 'SELECT * FROM Users WHERE age > ?'
+cursor.execute(query, (25,))
+users = cursor.fetchall()
+
+for user in users:
+    print(user)
+
+connection.close()
